48115ec8af96854e95ecbc0f4f4d63011e626747
David Hibbitts <daveh@imaginariumuk.com>
2017-01-15 15:43:51 +0000

    Main now supports command line arguments

--- Main.py
+++ Main.py
@@ -1,23 +1,50 @@
 import json
 import OBJ_Reader
+import os, glob
+import multiprocessing
+from functools import partial
+
+def OutputName(inFile):
+    return inFile.rsplit('.', 1)[0] + ".txt"
 
 def SaveLevel(level, outFile):
     with open(outFile, 'wb') as f:
         json.dump(level.toJSON(), f, indent=4, separators=(',', ': '))
     return True
 
-def ConvertFile(inFile, outFile):
+def ConvertFile(inFile, isVerbose):
+    print "Converting: {}".format(inFile)
+    outFile = OutputName(inFile)
     extension = inFile.rsplit('.', 1)[-1]
     level = None
     if extension.lower() == "obj":
         print "OBJ File found"
-        level = OBJ_Reader.OBJToLevel(inFile)
+        level = OBJ_Reader.OBJToLevel(inFile, isVerbose)
 
     if level is None:
         return False
 
-    print json.dumps(level.toJSON(), indent=4, separators=(',', ': '))
+    if isVerbose: json.dumps(level.toJSON(), indent=4, separators=(',', ': '))
     return SaveLevel(level, outFile)
+
+def ConvertFolder(folderPath, doRecurse, doParallel, isVerbose):
+    if doParallel:
+        parpool = multiprocessing.Pool()
+        map_func = lambda x, y: parpool.map(x, y)
+    else:
+        map_func = lambda x, y: map(x, y)
+
+    if doRecurse:
+        files = []
+        for root, dirnames, filenames in os.walk(folderPath):
+            files += map(lambda x: os.path.join(root, x), filenames)
+    else:
+        files = glob.glob(folderPath + "/*.*")
+
+    map_func(partial(ConvertFile, isVerbose=isVerbose), files)
+
+    if doParallel:
+        parpool.close()
 
 def Main():
     inFile = "Example Levels/Bare_minimum/Bare_minimum.obj"
@@ -25,4 +52,32 @@ def Main():
     print ConvertFile(inFile, outFile)
 
 if __name__ == '__main__':
-    Main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose",
+                        help="Verbose mode. Whether to print additional processing details.",
+                        action="store_true",
+                        default=False)
+    parser.add_argument("--parallel",
+                        help="Parallel mode. Whether to use multiprocessing for handling multiple files.",
+                        action="store_true",
+                        default=False)
+    parser.add_argument("-f", "--file",
+                        help="File mode. It will convert a specified file",
+                        type=str,
+                        default="")
+    parser.add_argument("-r", "--recursive",
+                        help="Recursive mode. Directory mode will now walk through sub folders. Has no effect if in File mode",
+                        action="store_true",
+                        default=False)
+    parser.add_argument("-p", "--path",
+                        help="A valid path. A directory path by default or a filepath in File mode. Defaults to the script folder",
+                        default=os.path.dirname(os.path.realpath(__file__)),
+                        required=False)
+    args = parser.parse_args()
+    assert os.path.exists(args.path)
+    if args.file:
+        print "File Mode:\n"
+        ConvertFile(args.path, args.verbose)
+    else:
+        ConvertFolder(args.path, args.recursive, args.parallel,args.verbose)
--- OBJ_Reader.py
+++ OBJ_Reader.py
@@ -3,11 +3,11 @@ import Blocks
 import numpy as np
 import Core
 
-def OBJToLevel(inFile):
-    return ParseOBJ(inFile)
+def OBJToLevel(inFile, isVerbose=False):
+    return ParseOBJ(inFile, isVerbose)
 
 
-def ParseOBJ(inFile):
+def ParseOBJ(inFile, verbose=False):
     with open(inFile, 'rb') as f:
         objLines = f.readlines()
     material = None
@@ -18,22 +18,22 @@ def ParseOBJ(inFile):
         lineType = lineType.lower()
         if lineType == "usemtl":
             material = str.strip(data).split(".", 1)[0]
-            print "Using Material: {}".format(material)
+            if verbose: print "Using Material: {}".format(material)
         elif lineType == "o":
             if len(verts) == 8:
-                print "Block Found."
+                if verbose: print "Block Found."
                 if material in Blocks.STATIC_BLOCKS:
                     pointList = np.array(verts, dtype=float)
                     size, position, rotation = Core.CalculateBlockProperties(pointList)
                     static_blocks.append(Blocks.Block(material, size, position, rotation, False, False, False))
-            print "\n----------\n"
+            if verbose: print "\n----------\n"
 
-            print "{} found".format(data)
+            if verbose: print "{} found".format(data)
             verts = []
         elif lineType == "v":
             verts.append(np.array(data.split(), dtype=float))
     if len(verts) == 8:
-        print "Block Found."
+        if verbose: print "Block Found."
         if material in Blocks.STATIC_BLOCKS:
             pointList = np.array(verts, dtype=float)
             size, position, rotation = Core.CalculateBlockProperties(pointList)
